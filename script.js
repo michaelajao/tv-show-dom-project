ec2fbedbdc0a32e95c5458ba31a01da9e2e648c4
Michael Ajao-olarinoye <ajaoolarinoyemichael@gmail.com>
2021-02-14 10:51:41 +0000

    complete level 100 with css design

--- script.js
+++ script.js
@@ -6,17 +6,34 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const container = document.getElementById("episodes");
-  console.log(episodeList);
 
   episodeList.forEach(allEpisodes => {
 
+    const card = document.createElement("div");
+    const h1 = document.createElement("hi");
+    const p = document.createElement("p");
     const img = document.createElement("img");
+
+    card.classList.add("card");
+    h1.innerHTML = `${allEpisodes.name} - ${makeEpisodeCode(allEpisodes)}`;
+    p.innerHTML = allEpisodes.summary;
     img.setAttribute("src", allEpisodes.image.medium);
 
-    container.appendChild(img);
+    container.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(img);
+    card.appendChild(p);
 
   });
 
+}
+
+function pad(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function makeEpisodeCode(allEpisodes){
+  return `S${pad(allEpisodes.season)}E${pad(allEpisodes.number)}`;
 }
 
 window.onload = setup;
